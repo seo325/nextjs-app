d03438fdf85795543a1ac8aed825832bf27256c9
seohwa0325 <seohwa0325@gmail.com>
2023-08-14 10:35:01 +0900

    nextjs app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import homeStyles from '../../styles/Home.module.css'
 
 const Home :NextPage = () =>{
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title> Your Name</title>
       </Head>
-      <section>
+      <section className={homeStyles.headingMd}>
         <p> your self introduction</p>
         <p> this is website</p>
       </section>
 
-      <section>
-        <h2> blog </h2>
-        <ul>
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+        <h2 className={homeStyles.headingLg}> blog </h2>
+        <ul className={homeStyles.list}>
 
 
         </ul>
